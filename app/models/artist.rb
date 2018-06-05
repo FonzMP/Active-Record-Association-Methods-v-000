e79fa0e116b9368a6fa0413d8ef38b3da0c56a17
Alex Wilson <fonzmp@gmail.com>
2018-06-05 19:57:10 +0000

    automatically backed up by learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -13,7 +13,7 @@ class Artist < ActiveRecord::Base
 
   def song_count
     #return the number of songs associated with the artist
-    artist_songs = self.songs
+    artist_songs = self.songs.count
   end
 
   def genre_count
