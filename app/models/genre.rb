cfaad3142e6f78cf1aac9d8832e6880ac1938f27
Alex Wilson <fonzmp@gmail.com>
2018-06-05 19:59:56 +0000

    automatically backed up by learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -14,7 +14,6 @@ class Genre < ActiveRecord::Base
 
   def all_artist_names
     # return an array of strings containing every musician's name
-    artist = self.artists
-    artist.name
+    self.artists
   end
 end
